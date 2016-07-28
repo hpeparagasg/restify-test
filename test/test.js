6caf11ee0ae4da66f4802852c4ab00ddd1c2f009
Adriell Matthew Julius Dagasuan <adriellmatthewjuliusdagasuan@amjdagasuan.local>
2016-07-28 19:09:44 +0800

    added test scripts

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var should = require('should');
+var expect = require('chai').expect;
+var request = require('request');
+var baseUrl = 'http://localhost:3000/';
+
+describe('returns recipes', function(){
+  it('returns recipes', function(done) {
+    request.get({url: baseUrl + 'recipes'}, function (error, response, body){
+      expect(response.statusCode).to.equal(200);
+    });
+    done();
+  });
+});
+
+describe('adds new recipe',function(){
+  it('adds new recipe', function(done){
+    request.post({url: baseUrl + 'recipes', formData:{'name':'Igado','ingredients':['Pork','Vinegar','Peas','Pork Liver']}},
+          function (error, response, body){
+            expect(response.statusCode).to.equal(201);
+          });
+    done();
+  })
+})
